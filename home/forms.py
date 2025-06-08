9c3c2184f54c5b3bf1b9475cdebfdf38c6c20e76
McaCoders2025 <overlord07062002@gmail.com>
2025-06-08 10:36:13 +0530

    Update forms.py

--- home/forms.py
+++ home/forms.py
@@ -7,14 +7,14 @@ from .models import UploadFileForm
 class StudForm(ModelForm):
     class Meta:
         model=Student
-        fields=["rollnumber","name","age","email","address"]
+        fields=[ "rollnumber", "name", "age", "email", "address",
+            "physics", "chemistry", "maths", "english",
+            "totalmarks", "maxmarks", "percentage"]
 
 
 class UploadFileForm(forms.ModelForm):
     class Meta:
         model = UploadFileForm
         fields = ['file']        
-
-
 
 
