19ccbc8406ae649f36780b9fd4b3dbedbbf48ef4
McaCoders2025 <overlord07062002@gmail.com>
2025-06-08 11:13:58 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -18,7 +18,8 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
-    path('send-email/', views.send_email_to_student, name="send-email"),
+    #path('send-email/', views.send_email_to_student, name="send-email"),
     path('download/<rollnumber>/', views.download_marksheet, name='download_marksheet'),
+    path('send-email/<int:rollnumber>/', views.send_email_to_student, name='send_email_to_student'),
     
 ]
